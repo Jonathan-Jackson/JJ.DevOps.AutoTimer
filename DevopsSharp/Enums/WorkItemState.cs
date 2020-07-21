d139dbf5f6bf088eac0c560c2d6cb588da7e8a12
Jon <jonathan.jackson@sgsco.com>
2020-07-21 21:35:25 +0100

    Removed legacy code.

--- DevopsSharp/Enums/WorkItemState.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DevopsSharp.Enums {
-
-    public enum WorkItemState {
-        New = 0,
-        Active = 1,
-        Closed = 2
-    }
-}
--- DevopsSharp/Enums/WorkItemType.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DevopsSharp.Enums {
-
-    public enum WorkItemType {
-        Bug = 0,
-        UserStory = 1,
-        Feature = 2,
-        Task = 3
-    }
-}
